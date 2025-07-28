c31995ce97bf340055029da9fbc93cc950a87246
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 23:29:47 +0900

    feat : Banner style add

---
+++ src/features/fade/ui/FadeBanner.tsx
@@ -0,0 +1,22 @@
+// components/Banner.tsx
+'use client';
+
+import { useEffect, useState } from 'react';
+import * as styles from './FadeBanner.css';
+export default function Banner() {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(false);
+        }, 3000); // 5초 후 사라지게
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className={`${styles.banner} ${isVisible ? styles.bannerFadeIn : styles.bannerFadeOut}`}>
+            🎉 지금 가입하면 30% 할인!
+        </div>
+    );
+}
