a36e17f78ff449c31f51683bb76823c9f5a62696
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 14:20:59 +0900

    feat : FadeTwo 추가

--- src/widgets/fadeWidget/FadeWidget.tsx
+++ src/widgets/fadeWidget/FadeWidget.tsx
@@ -1,5 +1,11 @@
 import Fade from '@/features/fade/ui/Fade';
+import FadeTwo from '@/features/fade/ui/FadeTwo';
 
 export default function FadeWidget() {
-    return <Fade />;
+    return (
+        <div>
+            <Fade />
+            <FadeTwo />
+        </div>
+    );
 }
