acd9e7c5765e90e56e89a3e76eeb3690ec344673
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 23:30:15 +0900

    feat : Fade banner widget 에 추가

--- src/widgets/fadeWidget/FadeWidget.tsx
+++ src/widgets/fadeWidget/FadeWidget.tsx
@@ -1,9 +1,11 @@
 import Fade from '@/features/fade/ui/Fade';
+import FadeBanner from '@/features/fade/ui/FadeBanner';
 import FadeTwo from '@/features/fade/ui/FadeTwo';
 
 export default function FadeWidget() {
     return (
         <div>
+            <FadeBanner />
             <Fade />
             <FadeTwo />
         </div>
