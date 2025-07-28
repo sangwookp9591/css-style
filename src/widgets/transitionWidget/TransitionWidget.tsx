b332d54276de06498b0a29a4956b1829bf780c5e
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 15:09:55 +0900

    feat : TransitionSlide 컴포넌트 위젯에 추가

--- src/widgets/transitionWidget/TransitionWidget.tsx
+++ src/widgets/transitionWidget/TransitionWidget.tsx
@@ -1,9 +1,11 @@
 import Transition from '@/features/transition/ui/Transition';
+import TransitionSlide from '@/features/transition/ui/TransitionSlide';
 
 export default function TransitionWidget() {
     return (
         <div>
             <Transition />
+            <TransitionSlide />
         </div>
     );
 }
