03deb3aa770efd1525ea812394c4ee9596e3392d
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 16:56:18 +0900

    feat : skeleton page에 추가

---
+++ src/app/animation/skeleton/page.tsx
@@ -0,0 +1,9 @@
+import SkeletonWidget from '@/widgets/skeletonWidget/SkeletonWidget';
+
+export default function SkeletonPage() {
+    return (
+        <div>
+            <SkeletonWidget />
+        </div>
+    );
+}
