c36eaf44429abf5656257b10f9c5a1f7360887b1
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 19:40:29 +0900

    feat : hoverCard component 추가

---
+++ src/features/scale/ui/HoverScale.tsx
@@ -0,0 +1,7 @@
+import * as styles from './HoverScale.css';
+
+export default function HoverScale() {
+    return (
+        <div className={styles.hoverCard} style={{ border: '1px solid black', height: '300px', width: '300px' }}></div>
+    );
+}
