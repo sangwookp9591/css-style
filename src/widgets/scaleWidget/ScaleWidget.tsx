a6e279239633c88720d0e4f11bdac9d2f9bf2558
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 21:40:21 +0900

    feat : ScaleWidget 추가

--- src/widgets/scaleWidget/ScaleWidget.tsx
+++ src/widgets/scaleWidget/ScaleWidget.tsx
@@ -1,11 +1,13 @@
 import HoverScale from '@/features/scale/ui/HoverScale';
 import Scale from '@/features/scale/ui/Scale';
+import ScaleBounce from '@/features/scale/ui/ScaleBounce';
 
 export default function ScaleWidget() {
     return (
         <div>
             <Scale />
             <HoverScale />
+            <ScaleBounce />
         </div>
     );
 }
