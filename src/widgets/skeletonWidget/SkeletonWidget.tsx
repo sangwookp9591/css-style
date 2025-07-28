c25ebe6338edd98351d58590cb2111962c2760c1
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 16:56:08 +0900

    feat : skeleton component 위젯 추가

---
+++ src/widgets/skeletonWidget/SkeletonWidget.tsx
@@ -0,0 +1,9 @@
+import Skeleton from '@/features/skeleton/ui/Skeleton';
+
+export default function SkeletonWidget() {
+    return (
+        <div>
+            <Skeleton />
+        </div>
+    );
+}
