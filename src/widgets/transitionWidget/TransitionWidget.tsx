995243a88dc38867077518f5210a8345d8d1557c
sangwookpark <sangwookp9591@gmail.com>
2025-07-28 22:45:46 +0900

    Feat : TransitionWidget에 tranisitionRise 등록

--- src/widgets/transitionWidget/TransitionWidget.tsx
+++ src/widgets/transitionWidget/TransitionWidget.tsx
@@ -1,11 +1,13 @@
 import Dropdown from '@/features/transition/ui/Dropdown';
 import PageEnterExit from '@/features/transition/ui/PageEnterExit';
 import Transition from '@/features/transition/ui/Transition';
+import TransitionRise from '@/features/transition/ui/TransitionRise';
 import TransitionSlide from '@/features/transition/ui/TransitionSlide';
 
 export default function TransitionWidget() {
     return (
         <div>
+            <TransitionRise />
             <Transition />
             <TransitionSlide />
             <Dropdown />
