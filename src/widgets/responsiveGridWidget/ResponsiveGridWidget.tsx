9cb3cc72620784dea1f1910966f4c16ce7bfa353
sangwookpark <sangwookp9591@gmail.com>
2025-07-29 19:13:26 +0900

    feat: 반응형 메뉴에 widget추가

--- src/widgets/responsiveGridWidget/ResponsiveGridWidget.tsx
+++ src/widgets/responsiveGridWidget/ResponsiveGridWidget.tsx
@@ -1,5 +1,11 @@
 import ResponsiveGrid from '@/features/responsive/ui/ResponsiveGrid';
+import ResponsiveMenu from '@/features/responsive/ui/ResponsiveMenu';
 
 export default function ResponsiveGridWidget() {
-    return <ResponsiveGrid />;
+    return (
+        <div>
+            <ResponsiveMenu />
+            <ResponsiveGrid />
+        </div>
+    );
 }
