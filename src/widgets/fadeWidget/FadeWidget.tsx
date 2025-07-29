41242fefd192b93360b322fac8f5399f715142eb
sangwookpark <sangwookp9591@gmail.com>
2025-07-29 15:36:17 +0900

    feat : BannerTwo widget에 add

--- src/widgets/fadeWidget/FadeWidget.tsx
+++ src/widgets/fadeWidget/FadeWidget.tsx
@@ -1,11 +1,13 @@
 import Fade from '@/features/fade/ui/Fade';
 import FadeBanner from '@/features/fade/ui/FadeBanner';
+import FadeBannerTwo from '@/features/fade/ui/FadeBannerTwo';
 import FadeTwo from '@/features/fade/ui/FadeTwo';
 
 export default function FadeWidget() {
     return (
         <div>
-            <FadeBanner />
+            {/* <FadeBanner /> */}
+            <FadeBannerTwo />
             <Fade />
             <FadeTwo />
         </div>
