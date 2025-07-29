03cd6c88562fa329ccacab23758239501c3a2d1f
sangwookpark <sangwookp9591@gmail.com>
2025-07-29 17:02:31 +0900

    feat : responsive theme 추가

---
+++ src/shared/style/theme.css.ts
@@ -0,0 +1,5 @@
+export const breakpoints = {
+    mobile: 'screen and (max-width: 767px)',
+    tablet: 'screen and (min-width: 768px) and (max-width: 1023px)',
+    desktop: 'screen and (min-width: 1024px)',
+};
