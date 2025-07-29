07ae1a7c0d477ed6e1ef62d05bed51e031dc3391
sangwookpark <sangwookp9591@gmail.com>
2025-07-29 19:14:56 +0900

    feat : LayoutChange compoennt 추가

---
+++ src/features/responsive/ui/ResponsiveLayoutChange.tsx
@@ -0,0 +1,13 @@
+import * as styles from './ResponsiveLayoutChange.css';
+
+export default function ResponsiveMenu() {
+    return (
+        <div>
+            {/*  레이아웃 위치 순서 바꾸기 */}
+            <section className={styles.flexSection}>
+                <div className={styles.textBlock}>📝 설명 텍스트</div>
+                <div className={styles.imageBlock}>🖼️ 이미지</div>
+            </section>
+        </div>
+    );
+}
