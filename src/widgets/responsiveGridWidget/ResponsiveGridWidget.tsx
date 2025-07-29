6d257305368d66772250741b9f3d5452890215df
sangwookpark <sangwookp9591@gmail.com>
2025-07-29 19:14:37 +0900

    feat : ResponsiveTableToList 를 위젯에 추가

--- src/widgets/responsiveGridWidget/ResponsiveGridWidget.tsx
+++ src/widgets/responsiveGridWidget/ResponsiveGridWidget.tsx
@@ -1,10 +1,12 @@
 import ResponsiveGrid from '@/features/responsive/ui/ResponsiveGrid';
 import ResponsiveMenu from '@/features/responsive/ui/ResponsiveMenu';
+import ResponsiveTableToList from '@/features/responsive/ui/ResponsiveTableToList';
 
 export default function ResponsiveGridWidget() {
     return (
         <div>
             <ResponsiveMenu />
+            <ResponsiveTableToList />
             <ResponsiveGrid />
         </div>
     );
