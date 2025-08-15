8b3c48bda223b653391d8644faded00a9746525c
sangwookpark <sangwookp9591@gmail.com>
2025-08-15 19:58:14 +0900

    feat : 주말ㅇㅅㅇ docs

--- src/widgets/scaleWidget/ScaleWidget.tsx
+++ src/widgets/scaleWidget/ScaleWidget.tsx
@@ -2,6 +2,7 @@ import HoverScale from '@/features/scale/ui/HoverScale';
 import Scale from '@/features/scale/ui/Scale';
 import ScaleBounce from '@/features/scale/ui/ScaleBounce';
 
+//s
 export default function ScaleWidget() {
     return (
         <div>
